ec8336ed0651720c937160cf2b8b79beaeae4c5b
chetansinghania <chetanFEB09@gmail.com>
2026-05-28 10:21:12 +0530

    clean final fyers setup

--- zerodha_dashboard.py
+++ zerodha_dashboard.py
@@ -6,13 +6,12 @@ st.title("LIVE F&O SCREENER")
 
 client_id = "G5H4DU2N1A-100"
 
-access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJHNUg0RFUyTjFBIiwidXVpZCI6ImU1NjlkNTQxZDc1ZDRlOTdiMjMyZmU1ZDY1YTUzYmUwIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhDMDgxNjEiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJmZmUzZGIzNjk1NmU5ZWU3ZThmOGVlMzA3MDE4NDVhMDYzMmZmOTIwMTNhNWI0ZWU4YTVjNzY4MiIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiXSIsImV4cCI6MTc3OTk3MjEyNCwiaWF0IjoxNzc5OTQyMTI0LCJpc3MiOiJhcGkubG9naW4uZnllcnMuaW4iLCJuYmYiOjE3Nzk5NDIxMjQsInN1YiI6ImF1dGhfY29kZSJ9.lDj_85JhFAW4QCYNOVGSYpI9lbEUacJKv6zX-AJfptE"
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJHNUg0RFUyTjFBIiwidXVpZCI6ImU1NjlkNTQxZDc1ZDRlOTdiMjMyZmU1ZDY1YTUzYmUwIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhDMDgxNjEiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJmZmUzZGIzNjk1NmU5ZWU3ZThmOGVlMzA3MDE4NDVhMDYzMmZmOTIwMTNhNWI0ZWU4YTVjNzY4MiIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiXSIsImV4cCI6MTc3OTk3MjEyNCwiaWF0IjoxNzc5OTQyMTI0LCJpc3MiOiJhcGkubG9naW4uZnllcnMuaW4iLCJuYmYiOjE3Nzk5NDIxMjQsInN1YiI6ImF1dGhfY29kZSJ9.lDj_85JhFAW4QCYNOVGSYpI9lbEUacJKv6zX-AJfptE"
 
 fyers = fyersModel.FyersModel(
     client_id=client_id,
-    token=access_token,
-    is_async=False,
-    log_path=""
+    token=token,
+    is_async=False
 )
 
 stocks = [
@@ -27,22 +26,18 @@ for stock in stocks:
 
     try:
 
-        data = {
+        response = fyers.quotes({
             "symbols": stock
-        }
+        })
 
-        response = fyers.quotes(data)
-
-        if response.get("s") != "ok":
-            raise Exception(str(response))
+        if response["s"] != "ok":
+            raise Exception(response)
 
         stock_data = response["d"][0]["v"]
 
-        lp = stock_data.get("lp", 0)
-
         results.append({
             "STOCK": stock,
-            "PRICE": lp
+            "PRICE": stock_data.get("lp")
         })
 
     except Exception as e:
