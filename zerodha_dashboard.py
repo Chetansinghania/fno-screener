d31da9ea6047914bb50a2c45e590576cc105e541
chetansinghania <chetanFEB09@gmail.com>
2026-05-28 10:16:40 +0530

    using official fyers sdk

--- zerodha_dashboard.py
+++ zerodha_dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import requests
+from fyers_apiv3 import fyersModel
 
 st.title("LIVE F&O SCREENER")
 
@@ -8,7 +8,12 @@ client_id = "G5H4DU2N1A-100"
 
 access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJHNUg0RFUyTjFBIiwidXVpZCI6ImU1NjlkNTQxZDc1ZDRlOTdiMjMyZmU1ZDY1YTUzYmUwIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhDMDgxNjEiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJmZmUzZGIzNjk1NmU5ZWU3ZThmOGVlMzA3MDE4NDVhMDYzMmZmOTIwMTNhNWI0ZWU4YTVjNzY4MiIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiXSIsImV4cCI6MTc3OTk3MjEyNCwiaWF0IjoxNzc5OTQyMTI0LCJpc3MiOiJhcGkubG9naW4uZnllcnMuaW4iLCJuYmYiOjE3Nzk5NDIxMjQsInN1YiI6ImF1dGhfY29kZSJ9.lDj_85JhFAW4QCYNOVGSYpI9lbEUacJKv6zX-AJfptE"
 
-full_token = f"{client_id}:{access_token}"
+fyers = fyersModel.FyersModel(
+    client_id=client_id,
+    token=access_token,
+    is_async=False,
+    log_path=""
+)
 
 stocks = [
     "NSE:RELIANCE-EQ",
@@ -22,21 +27,16 @@ for stock in stocks:
 
     try:
 
-        url = f"https://api-t1.fyers.in/data/quotes?symbols={stock}"
+        data = {
+            "symbols": stock
+        }
 
-        response = requests.get(
-            url,
-            headers={
-                "Authorization": full_token
-            }
-        )
+        response = fyers.quotes(data)
 
-        data = response.json()
+        if response.get("s") != "ok":
+            raise Exception(str(response))
 
-        if data.get("s") != "ok":
-            raise Exception(str(data))
-
-        stock_data = data["d"][0]["v"]
+        stock_data = response["d"][0]["v"]
 
         lp = stock_data.get("lp", 0)
 
