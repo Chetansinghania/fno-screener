9fcce02b9fc53d3ea6a72b933dca4538fdf45343
chetansinghania <chetanFEB09@gmail.com>
2026-05-28 10:10:53 +0530

    fixed indentation

--- zerodha_dashboard.py
+++ zerodha_dashboard.py
@@ -6,12 +6,26 @@ st.title("LIVE F&O SCREENER")
 
 access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOiJHNUg0RFUyTjFBIiwidXVpZCI6ImU1NjlkNTQxZDc1ZDRlOTdiMjMyZmU1ZDY1YTUzYmUwIiwiaXBBZGRyIjoiIiwibm9uY2UiOiIiLCJzY29wZSI6IiIsImRpc3BsYXlfbmFtZSI6IlhDMDgxNjEiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJmZmUzZGIzNjk1NmU5ZWU3ZThmOGVlMzA3MDE4NDVhMDYzMmZmOTIwMTNhNWI0ZWU4YTVjNzY4MiIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImF1ZCI6IltcImQ6MVwiLFwiZDoyXCIsXCJ4OjBcIixcIng6MVwiXSIsImV4cCI6MTc3OTk3MjEyNCwiaWF0IjoxNzc5OTQyMTI0LCJpc3MiOiJhcGkubG9naW4uZnllcnMuaW4iLCJuYmYiOjE3Nzk5NDIxMjQsInN1YiI6ImF1dGhfY29kZSJ9.lDj_85JhFAW4QCYNOVGSYpI9lbEUacJKv6zX-AJfptE"
 
-response = requests.get(
-    url,
-    headers={
-        "Authorization": access_token
-    }
-)
+stocks = [
+    "NSE:RELIANCE-EQ",
+    "NSE:HDFCBANK-EQ",
+    "NSE:SBIN-EQ"
+]
+
+results = []
+
+for stock in stocks:
+
+    try:
+
+        url = f"https://api-t1.fyers.in/data/quotes?symbols={stock}"
+
+        response = requests.get(
+            url,
+            headers={
+                "Authorization": access_token
+            }
+        )
 
         data = response.json()
 
